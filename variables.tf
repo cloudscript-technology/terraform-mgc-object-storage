8c752e1e5fb2385acece3d59e96fb7a18dbcdf4c
Jonathan Schmitt <jonathanschmitt.blog@gmail.com>
2024-07-05 17:37:03 -0300

    chore: Update variable descriptions and defaults in variables.tf

--- variables.tf
+++ variables.tf
@@ -11,16 +11,19 @@ variable "bucket_is_prefix" {
 variable "authenticated_read" {
   type        = bool
   description = "Owner gets FULL_CONTROL. Authenticated users have READ rights"
+  default     = null
 }
 
 variable "aws_exec_read" {
   type        = bool
   description = ""
+  default     = null
 }
 
 variable "enable_versioning" {
   type        = bool
   description = "Enable versioning for this bucket"
+  default     = null
 }
 
 variable "grant_full_control" {
@@ -28,6 +31,7 @@ variable "grant_full_control" {
     id        = string
   }))
   description = "Grantees get FULL_CONTROL"
+  default     = null
 }
 
 variable "grant_read" {
@@ -35,6 +39,7 @@ variable "grant_read" {
     id        = string
   }))
   description = "Allows grantees to list the objects in the bucket"
+  default     = null
 }
 
 variable "grant_read_acp" {
@@ -42,6 +47,7 @@ variable "grant_read_acp" {
     id        = string
   }))
   description = "Allows grantees to read the bucket ACL"
+  default     = null
 }
 
 variable "grant_write" {
@@ -49,6 +55,7 @@ variable "grant_write" {
     id        = string
   }))
   description = "Allows grantees to create objects in the bucket"
+  default     = null
 }
 
 variable "grant_write_acp" {
@@ -56,24 +63,29 @@ variable "grant_write_acp" {
     id        = string
   }))
   description = "Allows grantees to write the ACL for the applicable bucket"
+  default     = null
 }
 
 variable "private" {
   type        = bool
   description = "Owner gets FULL_CONTROL. Delegated users have access. No one else has access rights"
+  default     = null
 }
 
 variable "public_read" {
   type        = bool
   description = "Owner gets FULL_CONTROL. Everyone else has READ rights"
+  default     = null
 }
 
 variable "public_read_write" {
   type        = bool
   description = "Owner gets FULL_CONTROL. Everyone else has READ and WRITE rights"
+  default     = null
 }
 
 variable "recursive" {
   type        = bool
   description = "Delete bucket including objects inside"
+  default     = null
 }
